d94d13c18ce1e4677668e83705ed4f9cbd95ff91
Mel <132484274+llaala@users.noreply.github.com>
2025-08-01 08:56:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
